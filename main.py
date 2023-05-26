f01208893f3a300791a538f87d75303e56d0d8ee
Abhijoy-Mandal <a.mandal@mail.utoronto.ca>
2023-05-26 12:49:53 -0400

    completed video analysis code

--- main.py
+++ main.py
@@ -11,15 +11,30 @@ instance segmentation image with sam and clip with text prompts
 import os
 import os.path as ops
 import argparse
-
+from PIL import Image
+import imageio
 import cv2
 
 from local_utils.log_util import init_logger
 from local_utils.config_utils import parse_config_utils
 from models import build_sam_clip_text_ins_segmentor
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from visualizer import Visualizer
+from visualizer import ColorMode
+from detectron2.data import MetadataCatalog
 
+classes = ["Homogeneous Reaction", "Heterogeneous Reaction", "Residue", "Bubble", "Droplet", "Empty"]
+colors = [(189/255.0, 16/255.0, 224/255.0), (245/255.0, 166/255.0, 35/255.0), (110/255.0, 226/255.0, 105/255.0), (248/255.0, 231/255.0, 28/255.0), (0/255.0, 60/255.0, 255/255.0), (0/255.0, 60/255.0, 255/255.0)]
 
 LOG = init_logger.get_logger('instance_seg.log')
+MetadataCatalog.get("LLDataset").set(
+    thing_classes=["Homogeneous Reaction", "Heterogeneous Reaction", "Residue", "Bubble", "Droplet",
+                   "Empty"]).set(
+    thing_colors=[(189, 16, 224), (245, 166, 35), (110, 226, 105), (248, 231, 28), (0, 60, 255),
+                  (200, 200, 200)])
+LLDatasetMetadate = MetadataCatalog.get("LLDataset")
 
 
 def init_args():
@@ -85,6 +100,52 @@ def main():
 
     return
 
+def initialize_rcnn()
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 6
+    cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.1
+    cfg.MODEL.WEIGHTS = "2023_05_15-16_47_13_weights.pth"  # path to the model we just trained
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set a custom testing threshold
+    cfg.TEST.DETECTIONS_PER_IMAGE = 4
+    cfg.MODEL.DEVICE = "cuda"
+    predictor = DefaultPredictor(cfg)
+    return predictor
+
+
+def eval(im, boxes, predictor):
+    uncropped_outputs = predictor(im)
+    v_cropped = Visualizer(im,
+                             metadata=LLDatasetMetadate,
+                             scale=.6,
+                             instance_mode=ColorMode.SEGMENTATION
+                             # remove the colors of unsegmented pixels. This option is only available for segmentation models
+                             )
+    v_uncropped = Visualizer(im,
+                             metadata=LLDatasetMetadate,
+                             scale=.6,
+                             instance_mode=ColorMode.SEGMENTATION
+                             # remove the colors of unsegmented pixels. This option is only available for segmentation models
+                             )
+    for box in boxes:
+        x, y, w, h, = box
+        seg = im[y:y+h, x:x+w]
+        outputs = predictor(seg)
+        i = 0
+        for boxp in outputs["instances"].pred_boxes:
+            v_cropped.draw_box(boxp, edge_color=colors[outputs["instances"].pred_classes[i]])
+            v_cropped.draw_text(str(classes[outputs["instances"].pred_classes[i]]), tuple(box[:2].numpy()),
+                                  color=colors[outputs["instances"].pred_classes[i]])
+            i += 1
+
+    i = 0
+    for box in uncropped_outputs["instances"].pred_boxes:
+        v_uncropped.draw_box(box, edge_color=colors[uncropped_outputs["instances"].pred_classes[i]])
+        v_uncropped.draw_text(str(classes[uncropped_outputs["instances"].pred_classes[i]]), tuple(box[:2].numpy()),
+                    color=colors[uncropped_outputs["instances"].pred_classes[i]])
+        i += 1
+    return v_uncropped.get_output.get_image()[:, :, ::-1], v_cropped.get_output.get_image()[:, :, ::-1]
+
 
 def segment_video():
     args = init_args()
@@ -117,16 +178,32 @@ def segment_video():
     LOG.info('Segmentor initialized complete')
     LOG.info('Start to segment input image ...')
     ret = segmentor.seg_image("./data/test_images/one_frame_tmp.jpg", unique_label=unique_labels, use_text_prefix=use_text_prefix)
-    LOG.info(f'segment complete, masks found: {len(ret["raw_masks"])}')
-    LOG.info(f'segment complete, masks found: {len(ret["bbox"])}')
     masks = ret['raw_masks']
-    save_dir = args.save_dir
-    os.makedirs(save_dir, exist_ok=True)
-    mask_save_path = ops.join(save_dir, '{:s}_insseg_mask.png'.format(input_image_name.split('.')[0]))
-    cv2.imwrite(mask_save_path, ret['ins_seg_mask'])
+    vial_bbox = []
     for i in range(len(masks)):
-        LOG.info(f'Creating video segment {i+1} of {len(masks)}: {ret["bboxes_names"][i]}')
+        if ret["bboxes_names"][i]=="vial"
+            vial_bbox.append(ret["bbox"][i])
+    LOG.info(f'segment complete, masks found: {len(vial_bbox)}')
+    LOG.info(f'Initializing HeinSight2.0')
+    predictor = initialize_rcnn()
 
+    writer1 = imageio.get_writer(f'./output/insseg/uncrop_segment.mp4', fps=60)
+    writer2 = imageio.get_writer(f'./output/insseg/crop_segment.mp4', fps=60)
+    cap = cv2.VideoCapture(input_image_path)
+    LOG.info(f'Analysing Video')
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            uncrop_im, crop_im = eval(frame, vial_bbox, predictor)
+            writer1.append_data(uncrop_im)
+            writer2.append_data(crop_im)
+        else:
+            cap.release()
+            break
+    writer1.close()
+    writer2.close()
+    LOG.info(f'Videos saved at ./output/insseg/')
     # save cluster result
     # save_dir = args.save_dir
     # os.makedirs(save_dir, exist_ok=True)
